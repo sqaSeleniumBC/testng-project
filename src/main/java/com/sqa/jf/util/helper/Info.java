839129ad07fce2ced92a615428e1f7139ee3a37f
Jean-francois Nepton <jeanmfrancois@hotmail.com>
2016-02-10 19:13:54 -0800

    Create Info Class to Use For Informative Functions On All Objects

---
+++ src/main/java/com/sqa/jf/util/helper/Info.java
@@ -0,0 +1,42 @@
+/**
+ * File Name: Info.java<br>
+ * Nepton, Jean-francois<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor: Jean-francois Nepton<br>
+ * Created: Feb 10, 2016
+ */
+package com.sqa.jf.util.helper;
+
+/**
+ * Create a helper class to add to your framework which gives information on any
+ * class you create. The method signatures are below. Try to implement one at a
+ * time. Create a test case for each method.
+ *
+ * @author Nepton, Jean-francois
+ * @version 1.0.0
+ * @since 1.0
+ */
+public class Info {
+
+	public static void display2DInfo(Object[][] objects) {
+	}
+
+	public static void displayInfo(Object obj) {
+	}
+
+	public static void displayInfo(Object obj1, Object obj2, Object... objN) {
+	}
+
+	public static void displayInfo(Object[] obj) {
+	}
+
+	public static String get2DInfo(Object[][] objects) {
+		// TODO implement method to get 2d info
+		return null;
+	}
+
+	public static String getInfo(Object obj) {
+		// TODO implement method to get info
+		return null;
+	}
+}
